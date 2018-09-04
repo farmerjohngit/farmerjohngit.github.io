88930aa09d9e4331c949b3707792e1653aaebf13
zhangzunchang <zhangzunchang@u51.com>
2018-09-04 15:38:12 +0800

    update

--- up.sh
+++ up.sh
@@ -4,5 +4,6 @@ cp -rf /Users/zhangzunchang/Documents/blog/public/ /Users/zhangzunchang/new_blog
 cd /Users/zhangzunchang/new_blog_public
 git add -A
 git commit -m \"update\" 
-git push deploy --force
+git remote add deploy root@47.91.221.198:/root/blog_update
+git push deploy master --force
 #send "Farmercoding992#\r"
